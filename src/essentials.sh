959e140ffa97f67ca1fc679e2f622268dd1b309b
sharpordie <72373746+sharpordie@users.noreply.github.com>
2023-01-26 05:20:14 +0100

    small update

--- src/essentials.sh
+++ src/essentials.sh
@@ -1,28 +1,132 @@
+update_appearance() {
+
+	# Enable night-light
+	gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+	gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 0
+	gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to 0
+	gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 5000
+
+	# Change color-theme
+	gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+	gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
+
+	# Change icon-theme
+	sudo pacman -S --needed --noconfirm papirus-icon-theme
+	gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
+
+	# Change folder-theme
+	yay -S --needed --noconfirm papirus-folders
+	sudo papirus-folders --color yaru --theme Papirus-Dark
+
+	# Remove event-sounds
+	gsettings set org.gnome.desktop.sound event-sounds false
+
+	# Remove favorites
+	gsettings set org.gnome.shell favorite-apps "[]"
+
+}
+
 update_chromium() {
 
+	# Handle parameters
 	local deposit=${1:-$HOME/Downloads/DDL}
 	local startup=${2:-about:blank}
 
+	# Update dependencies
 	sudo pacman -S --needed --noconfirm curl jq ydotool
 
+	# Update package
 	local present=$([[ -n $(pacman -Q | grep ungoogled-chromium-bin) ]] && echo "true" || echo "false")
 	yay -S --needed --noconfirm ungoogled-chromium-bin
 
+	# Finish installation
 	if [[ "$present" == "false" ]]; then
-		break
-	fi
+		# Launch chromium
+		sleep 1 && (sudo ydotoold &) &>/dev/null
+		sleep 1 && (chromium &) &>/dev/null
+		sleep 4 && sudo ydotool key 125:1 103:1 103:0 125:0
 
-	if [[ "$present" == "false" ]]; then
+		# Change deposit
+		mkdir -p "$deposit"
+		sleep 1 && sudo ydotool key 29:1 38:1 38:0 29:0
+		sleep 1 && sudo ydotool type "chrome://settings/" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool type "before downloading" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 3); do sleep 0.5 && sudo ydotool key 15:1 15:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool key 56:1 15:1 15:0 56:0 && sleep 1 && sudo ydotool key 56:1 15:1 15:0 56:0
+		sleep 1 && sudo ydotool key 29:1 38:1 38:0 29:0 && sleep 1 && sudo ydotool type "$deposit" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool key 15:1 15:0 && sleep 1 && sudo ydotool key 28:1 28:0
+
+		# Change engine
+		sleep 1 && sudo ydotool key 29:1 38:1 38:0 29:0
+		sleep 1 && sudo ydotool type "chrome://settings/" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool type "search engines" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 3); do sleep 0.5 && sudo ydotool key 15:1 15:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool type "duckduckgo" && sleep 1 && sudo ydotool key 28:1 28:0
+
+		# Change custom-ntp
+		sleep 1 && sudo ydotool key 29:1 38:1 38:0 29:0
+		sleep 1 && sudo ydotool type "chrome://flags/" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool type "custom-ntp" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 5); do sleep 0.5 && sudo ydotool key 15:1 15:0; done
+		sleep 1 && sudo ydotool key 29:1 30:1 30:0 29:0 && sleep 1 && sudo ydotool type "$startup"
+		sleep 1 && for i in $(seq 1 2); do sleep 0.5 && sudo ydotool key 15:1 15:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool key 108:1 108:0 && sleep 1 && sudo ydotool key 28:1 28:0
+
+		# Change disable-sharing-hub
+		sleep 1 && sudo ydotool key 29:1 38:1 38:0 29:0
+		sleep 1 && sudo ydotool type "chrome://flags/" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool type "disable-sharing-hub" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 6); do sleep 0.5 && sudo ydotool key 15:1 15:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool key 108:1 108:0 && sleep 1 && sudo ydotool key 28:1 28:0
+
+		# Change extension-mime-request-handling
+		sleep 1 && sudo ydotool key 29:1 38:1 38:0 29:0
+		sleep 1 && sudo ydotool type "chrome://flags/" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool type "extension-mime-request-handling" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 6); do sleep 0.5 && sudo ydotool key 15:1 15:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 2); do sleep 0.5 && sudo ydotool key 108:1 108:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+
+		# Change hide-sidepanel-button
+		sleep 1 && sudo ydotool key 29:1 38:1 38:0 29:0
+		sleep 1 && sudo ydotool type "chrome://flags/" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool type "hide-sidepanel-button" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 6); do sleep 0.5 && sudo ydotool key 15:1 15:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool key 108:1 108:0 && sleep 1 && sudo ydotool key 28:1 28:0
+
+		# Change remove-tabsearch-button
+		sleep 1 && sudo ydotool key 29:1 38:1 38:0 29:0
+		sleep 1 && sudo ydotool type "chrome://flags/" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool type "remove-tabsearch-button" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 6); do sleep 0.5 && sudo ydotool key 15:1 15:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool key 108:1 108:0 && sleep 1 && sudo ydotool key 28:1 28:0
+
+		# Change show-avatar-button
+		sleep 1 && sudo ydotool key 29:1 38:1 38:0 29:0
+		sleep 1 && sudo ydotool type "chrome://flags/" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool type "show-avatar-button" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 6); do sleep 0.5 && sudo ydotool key 15:1 15:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 3); do sleep 0.5 && sudo ydotool key 108:1 108:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+
+		# Toggle bookmark bar (ctr+shift+b)
+		sleep 4 && sudo ydotool key 29:1 42:1 48:1 48:0 42:0 29:0
+
+		# Finish chromium
+		sleep 4 && sudo ydotool key 56:1 62:1 62:0 56:0
+
+		# Update chromium-web-store
 		local adjunct="NeverDecaf/chromium-web-store"
 		local address="https://api.github.com/repos/$adjunct/releases/latest"
 		local version=$(curl -LA "mozilla/5.0" "$address" | jq -r ".tag_name" | tr -d "v")
 		update_chromium_extension "https://github.com/$adjunct/releases/download/v$version/Chromium.Web.Store.crx"
+
+		# Update extensions
 		update_chromium_extension "bcjindcccaagfpapjjmafapmmgkkhgoa" # json-formatter
 		update_chromium_extension "ibplnjkanclpjokhdolnendpplpjiace" # simple-translate
 		update_chromium_extension "mnjggcdmjocbbbhaepdhchncahnbgone" # sponsorblock-for-youtube
 		update_chromium_extension "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock-origin
 	fi
 
+	# Update bypass-paywalls-chrome-clean
 	local address="https://gitlab.com/magnolia1234/bypass-paywalls-chrome-clean"
 	local address="$address/-/archive/master/bypass-paywalls-chrome-clean-master.zip"
 	update_chromium_extension "$address"
@@ -31,10 +135,13 @@ update_chromium() {
 
 update_chromium_extension() {
 
+	# Handle parameters
 	local payload=${1}
 
+	# Update dependencies
 	sudo pacman -S --needed --noconfirm curl libarchive ydotool
 
+	# Update extension
 	if [[ -x $(command -v chromium) ]]; then
 		pkill chromium
 		if [[ "$payload" == http* ]]; then
@@ -79,12 +186,15 @@ update_chromium_extension() {
 
 update_git() {
 
+	# Handle parameters
 	local default=${1:-main}
 	local gituser=${2}
 	local gitmail=${3}
 
+	# Update package
 	sudo pacman -S --needed --noconfirm git github-cli
 
+	# Change settings
 	git config --global http.postBuffer 1048576000
 	git config --global init.defaultBranch "$default"
 	if [[ -n "$gitmail" ]]; then git config --global user.email "$gitmail"; fi
@@ -94,25 +204,27 @@ update_git() {
 
 update_hashcat() {
 
-	sudo pacman -S --needed --noconfirm hashcat
-
+	# Update dependencies
 	if [[ $(lspci | grep -e VGA) == *"GeForce"* ]]; then sudo pacman -S --needed --noconfirm opencl-nvidia; fi
+
+	# Update package
+	sudo pacman -S --needed --noconfirm hashcat
 
 }
 
 update_jdownloader() {
 
+	# Handle parameters
 	local deposit=${1:-$HOME/Downloads/JD2}
 
+	# Update dependencies
 	sudo pacman -S --needed --noconfirm jdk-openjdk jq moreutils
 
+	# Update package
 	local present=$([[ -n $(pacman -Q | grep jdownloader2) ]] && echo "true" || echo "false")
 	yay -S --needed --noconfirm jdownloader2
 
-	if [[ "$present" == "false" ]]; then
-		update_chromium_extension "fbcohnmimjicjdomonkcbcpbpnhggkip" # myjdownloader-browser-ext
-	fi
-
+	# Finish installation
 	if [[ "$present" == "false" ]]; then
 		local appdata="$HOME/.jd/cfg"
 		local config1="$appdata/org.jdownloader.settings.GraphicalUserInterfaceSettings.json"
@@ -132,20 +244,177 @@ update_jdownloader() {
 		jq ".specialdealoboomdialogvisibleonstartup = false" "$config1" | sponge "$config1"
 		jq ".specialdealsenabled = false" "$config1" | sponge "$config1"
 		jq ".speedmetervisible = false" "$config1" | sponge "$config1"
-		jq ".defaultdownloadfolder = \"$deposit\"" "$config2" | sponge "$config2"
+		mkdir -p "$deposit" && jq ".defaultdownloadfolder = \"$deposit\"" "$config2" | sponge "$config2"
 		jq ".enabled = false" "$config3" | sponge "$config3"
 		jq ".enabled = false" "$config4" | sponge "$config4"
+		update_chromium_extension "fbcohnmimjicjdomonkcbcpbpnhggkip" # myjdownloader-browser-ext
 	fi
+
+}
+
+update_quickemu() {
+
+	# Update package
+	yay -S --needed --noconfirm quickemu
+
+}
+
+update_vmware_workstation() {
+
+	# Handle parameters
+	local deposit=${1:-$HOME/Machines}
+	local serials=${2:-MC60H-DWHD5-H80U9-6V85M-8280D}
+
+	# Update package
+	yay -S --needed --noconfirm vmware-workstation
+
+	# Enable services
+	sudo systemctl enable --now vmware-networks.service
+    sudo systemctl enable --now vmware-usbarbitrator.service
+
+	# Change serials
+	sudo /usr/lib/vmware/bin/vmware-vmx-debug --new-sn $serials
+
+	# Change settings
+	# TODO: Verify if first launch is required
+	configs="$HOME/.vmware/preferences"
+	if ! grep -q "prefvmx.defaultVMPath" "$configs" 2>/dev/null; then
+		echo "prefvmx.defaultVMPath = \"$deposit\"" >>"$configs"
+	fi
+
+}
+
+update_vscode() {
+
+	# Update dependencies
+	sudo pacman -S --needed --noconfirm jq moreutils
+
+	# Update package
+	yay -S --needed --noconfirm visual-studio-code-bin
+
+	# Update extensions
+	update_vscode_extension "bierner.markdown-preview-github-styles"
+	update_vscode_extension "foxundermoon.shell-format"
+	update_vscode_extension "github.github-vscode-theme"
+
+	# Change settings
+	configs="$HOME/.config/Code/User/settings.json"
+	[[ -s "$configs" ]] || echo "{}" >"$configs"
+	jq '."editor.fontFamily" = "Cascadia Code, monospace"' "$configs" | sponge "$configs"
+	jq '."editor.fontSize" = 13' "$configs" | sponge "$configs"
+	jq '."editor.lineHeight" = 35' "$configs" | sponge "$configs"
+	jq '."security.workspace.trust.enabled" = false' "$configs" | sponge "$configs"
+	jq '."telemetry.telemetryLevel" = "crash"' "$configs" | sponge "$configs"
+	jq '."update.mode" = "none"' "$configs" | sponge "$configs"
+	jq '."window.menuBarVisibility" = "toggle"' "$configs" | sponge "$configs"
+	jq '."workbench.colorTheme" = "GitHub Dark Default"' "$configs" | sponge "$configs"
+
+}
+
+update_vscode_extension() {
+
+	# Handle parameters
+	local payload=${1}
+
+	# Update extension
+	code --install-extension "$payload" --force &>/dev/null || true
 
 }
 
 main() {
 
-	sudo -v
-	update_chromium
-	update_git main sharpordie 72373746+sharpordie@users.noreply.github.com
-	update_hashcat
-	update_jdownloader
+	# Prompt password
+	sudo -v && clear
+
+	# Change headline
+	printf "\033]0;%s\007" "archogen"
+
+	# Output greeting
+	read -r -d "" welcome <<-EOD
+		░█████╗░██████╗░░█████╗░██╗░░██╗░█████╗░░██████╗░███████╗███╗░░██╗
+		██╔══██╗██╔══██╗██╔══██╗██║░░██║██╔══██╗██╔════╝░██╔════╝████╗░██║
+		███████║██████╔╝██║░░╚═╝███████║██║░░██║██║░░██╗░█████╗░░██╔██╗██║
+		██╔══██║██╔══██╗██║░░██╗██╔══██║██║░░██║██║░░╚██╗██╔══╝░░██║╚████║
+		██║░░██║██║░░██║╚█████╔╝██║░░██║╚█████╔╝╚██████╔╝███████╗██║░╚███║
+		╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝░╚════╝░░╚═════╝░╚══════╝╚═╝░░╚══╝
+	EOD
+	printf "\n\033[92m%s\033[00m\n\n" "$welcome"
+
+	# Remove timeouts
+	echo "Defaults timestamp_timeout=-1" | sudo tee "/etc/sudoers.d/disable_timeout" &>/dev/null
+
+	# Remove sleeping
+	gsettings set org.gnome.desktop.notifications show-banners false
+	gsettings set org.gnome.desktop.screensaver lock-enabled false
+	gsettings set org.gnome.desktop.session idle-delay 0
+
+	# Handle elements
+	factors=(
+		"update_appearance"
+		"update_system"
+		"update_git main sharpordie 72373746+sharpordie@users.noreply.github.com"
+		"update_ydotool"
+		"update_nvidia"
+
+		"update_android_studio"
+		"update_chromium"
+		"update_pycharm"
+		"update_vscode"
+
+		"update_docker"
+		"update_flutter"
+		"update_gh"
+		"update_mambaforge"
+		"update_nodejs"
+		"update_pgadmin"
+		"update_postgresql"
+		"update_python"
+
+		"update_converseen"
+		"update_darktable"
+		"update_figma"
+		"update_hashcat"
+		"update_inkscape"
+		"update_jdownloader"
+		"update_joal"
+		"update_keepassxc"
+		"update_lunacy"
+		"update_mkvtoolnix"
+		"update_mpv"
+		"update_odoo"
+		"update_quickemu"
+		"update_scrcpy"
+		"update_transmission"
+		"update_vmware_workstation"
+		"update_yt_dlp"
+	)
+
+	# Output progress
+	maximum=$((${#welcome} / $(echo "$welcome" | wc -l)))
+	heading="\r%-"$((maximum - 20))"s   %-6s   %-8s\n\n"
+	loading="\r%-"$((maximum - 20))"s   \033[93mACTIVE\033[0m   %-8s\b"
+	failure="\r%-"$((maximum - 20))"s   \033[91mFAILED\033[0m   %-8s\n"
+	success="\r%-"$((maximum - 20))"s   \033[92mWORKED\033[0m   %-8s\n"
+	printf "$heading" "FUNCTION" "STATUS" "DURATION"
+	for element in "${factors[@]}"; do
+		written=$(basename "$(echo "$element" | cut -d ' ' -f 1)" | tr "[:lower:]" "[:upper:]")
+		started=$(date +"%s") && printf "$loading" "$written" "--:--:--"
+		eval "$element" >/dev/null 2>&1 && current="$success" || current="$failure"
+		extinct=$(date +"%s") && elapsed=$((extinct - started))
+		elapsed=$(printf "%02d:%02d:%02d\n" $((elapsed / 3600)) $(((elapsed % 3600) / 60)) $((elapsed % 60)))
+		printf "$current" "$written" "$elapsed"
+	done
+
+	# Revert sleeping
+	gsettings set org.gnome.desktop.notifications show-banners true
+	gsettings set org.gnome.desktop.screensaver lock-enabled true
+	gsettings set org.gnome.desktop.session idle-delay 300
+
+	# Revert timeouts
+	sudo rm "/etc/sudoers.d/disable_timeout"
+
+	# Output new line
+	printf "\n"
 
 }
 
